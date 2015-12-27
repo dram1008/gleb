673d10b029bf6a48fa724450aa91327ee3c418ed
dram1008 <dram1008@yandex.ru>
2015-12-28 00:08:57 +0300

    init

--- views/site/about.php
+++ views/site/about.php
@@ -19,7 +19,6 @@ $page = \app\models\Page::find(1);
 
     <div class="one-third">
         <div class="outer none"><span class="inset"><a href="<?= \cs\Widget\FileUpload2\FileUpload::getOriginal($page->getField('image')) ?>" target="_blank"><img src="<?= $page->getField('image') ?>" ></a></span></div>
-        География ТПК
     </div>
 
     <div class="two-third last">
